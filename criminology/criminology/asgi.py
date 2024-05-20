3aab10b67dfedb4babe1a739b0811fc317969e66
Stephen <stephen.a.steyaert@gmail.com>
2024-05-20 17:39:13 -0400

    first commit

---
+++ criminology/criminology/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for criminology project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'criminology.settings')
+
+application = get_asgi_application()
